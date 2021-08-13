3b60f09fa1424b069b94e6b1a7db1f8d6d70692b
dkffkels116 <87690844+dkffkels116@users.noreply.github.com>
2021-08-13 14:52:57 +0900

    fix: delete password

--- Week3/auto_mail.py
+++ Week3/auto_mail.py
@@ -41,8 +41,8 @@ import re
 #### gmail 발송 기능에 필요한 계정 정보를 아래 코드에 입력하세요.
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 465
-SMTP_USER = 'dkffkels116@gmail.com'
-SMTP_PASSWORD = 'rlaalstjr1!'
+SMTP_USER = ''
+SMTP_PASSWORD = ''
 
 #### 아래 코드를 실행해 메일 발송에 필요한 send_mail 함수를 만드세요.
 def send_mail(name, addr, subject, contents, attachment=None):
